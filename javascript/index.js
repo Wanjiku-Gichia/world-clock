369f4fdf4f902f2a8b474d0b524c017b70797644
Wanjiku-Gichia <77983272+Wanjiku-Gichia@users.noreply.github.com>
2024-10-29 14:06:14 +0300

    imported moment.js to my HTML and  added JS functions for real-time date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
-
+function updateTime() {
+  let nairobiDateElement = document.querySelector("#nairobi .date");
+  nairobiDateElement.innerHTML = moment()
+    .tz("Africa/Nairobi")
+    .format("MMMM Do YYYY ");
+  let gothernburgDateElement = document.querySelector("#gothenburg .date");
+  gothernburgDateElement.innerHTML = moment()
+    .tz("Europe/Stockholm")
+    .format("MMMM Do YYYY ");
+  let nairobiTimeElement = document.querySelector("#nairobi .time");
+  nairobiTimeElement.innerHTML = moment()
+    .tz("Africa/Nairobi")
+    .format("H: mm: ss [<small>]A[</small>]");
+  let gothernburgTimeElement = document.querySelector("#gothenburg .time");
+  gothernburgTimeElement.innerHTML = moment()
+    .tz("Europe/Stockholm")
+    .format("H: mm: ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
